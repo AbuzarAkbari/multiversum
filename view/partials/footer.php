631c12892157be77640e56951612c329667bff22
abuzar-4a6c <abuzar_voetbal@msn.com>
2018-05-29 10:55:01 +0200

    betallings pagina

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -5,7 +5,6 @@
         <p class="m-0 text-center text-white">Multiversum &copy; All rights reserved | Design by Abuzar & Akram</p>
       </div> -->
       <!-- /.container -->
-    <!-- </footer>
 
     <!-- Bootstrap core JavaScript -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
